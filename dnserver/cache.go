82cc4363b5a30a1eda88a80a80ec452309f9d409
bigeagle <justin.w.xd@gmail.com>
2013-05-30 22:50:16 +0800

    data race eliminated

--- dnserver/cache.go
+++ dnserver/cache.go
@@ -3,6 +3,7 @@ package toydns
 import (
     "time"
     "strconv"
+    "sync"
 )
 
 type cacheItem struct {
@@ -15,6 +16,8 @@ type dnsCache struct {
     cache map[string] *cacheItem
 }
 
+var _lock sync.RWMutex
+
 func newDNSCache() *dnsCache {
     dnscache := new(dnsCache)
     dnscache.cache = make(map[string] *cacheItem, 0)
@@ -23,13 +26,17 @@ func newDNSCache() *dnsCache {
 
 func (self *dnsCache) Get(qname string, qtype int) ([]byte, bool) {
     key := qname + ":" + strconv.Itoa(qtype)
+    _lock.RLock()
     item, found := self.cache[key]
+    _lock.RUnlock()
     if ! found {
         return nil, false
     } else {
         nowts := time.Now().Unix()
         if nowts - item.ts >= int64(item.ttl) {
+            _lock.Lock()
             delete(self.cache, key)
+            _lock.Unlock()
             if log != nil {
                 log.Debug("Ttl expired")
             }
@@ -41,13 +48,17 @@ func (self *dnsCache) Get(qname string, qtype int) ([]byte, bool) {
 
 func (self *dnsCache) Insert(qname string, qtype int, pack []byte, ttl int) error {
     key := qname + ":" + strconv.Itoa(qtype)
+    _lock.RLock()
     _, found := self.cache[key]
+    _lock.RUnlock()
     if found {
         return nil
     } else {
         nowts := time.Now().Unix()
         citem := &cacheItem{ts: nowts, ttl: ttl, pack: pack}
+        _lock.Lock()
         self.cache[key] = citem
+        _lock.Unlock()
         return nil
     }
 }
